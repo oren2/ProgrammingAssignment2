c30069bef300a110b3c145f1aa8a2c7204359c4b
oren2 <orentini@outlook.com>
2019-08-22 17:23:05 +0300

    Write required Functions: 1. makeCacheMatrix 2. cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## functions to cache a solved matrix.
 
-## Write a short comment describing this function
+## special function to cache matrices
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  solved.Matrix <- NULL
+  
+  set.Matrix <- function(y){ #Sets new matrix.
+    x<<- y
+    solved.Matrix <<- NULL
+  }
+  
+  get.Matrix <- function() x  #Return x which is the matrix.
+  
+  set.solved.Matrix <- function(solved) solved.Matrix <<- solved
+  
+  get.solved.Matrix <- function() solved.Matrix
+  
+  list(
+    set.Matrix = set.Matrix,
+    get.Matrix = get.Matrix,
+    set.solved.Matrix = set.solved.Matrix,
+    get.solved.Matrix = get.solved.Matrix
+  )
+  
 }
 
 
-## Write a short comment describing this function
+## solves a Matrix and caches it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ solved.Matrix <- x$get.solved.Matrix()
+ if(!is.null(solved.Matrix)) {
+   message("getting cached solved matrix")
+   return(solved.Matrix)
+ }
+ 
+ data <- x$get.Matrix()
+ solved.Matrix <- solve(data,...)
+ x$set.solved.Matrix (solved.Matrix)
+ solved.Matrix
 }
